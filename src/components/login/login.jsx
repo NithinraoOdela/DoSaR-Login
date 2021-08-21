115dfa6edfb54346ac67f440b35fbb447123bbd4
NithinraoOdela <65495762+NithinraoOdela@users.noreply.github.com>
2021-08-21 15:19:34 +0530

    Update login.jsx

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -12,8 +12,47 @@ export class Login extends React.Component {
 
     handlerclick = () => {
 
-        alert('Submitted')
-        window.open('https://github.com/Github', '_self')
+        // window.open('https://nithinraoodela.github.io/Registration/', '_self')
+        
+        
+        var un = document.getElementById("isd").value
+        var ul = document.getElementById("isdd").value
+        // var uk = document.getElementById("btt")
+        if (un  === "2019A8PS0429G" && ul === "Nithinrao@123" ){  //Nithin
+            
+            window.open('https://nithinraoodela.github.io/Registration/', '_self')
+        }
+        else if (un  === "2019A4PS0501G" && ul === "Nithinrao@123" ){  //CHARAN YADAV
+            
+            
+            window.open("https://nithinraoodela.github.io/Registration/",'_self')
+            
+        }
+        else if (un  === "2019A7PS0124G" && ul === "Nithinrao@123" ){ //Addepalli Guru Sai Manikanta Bhaskar
+            window.open('ind4.html', "_blank")
+        }
+        else if (un  === "2019A4PS0436G" && ul === "Nithinrao@123" ){ //MATSA SAI SURESHKUMAR  
+            window.open('ind5.html') 
+        }
+        else{
+            if (un ==="") {
+
+                alert("Blank / Invalid ID")
+            }
+            else{
+
+                alert("invalid Password")
+            }
+            
+        }
+
+
+
+       
+
+
+         
+
 
 
     }
@@ -32,17 +71,17 @@ export class Login extends React.Component {
                         <div className="form">
                             <div className="form-group">
                                 <label htmlFor="Campus ID">Campus ID</label>
-                                <input type="text" name="Campus ID" placeholder="20**A*PS****G" ref={this.inputRef}required/>
+                                <input type="text" name="Campus ID" id="isd" placeholder="20**A*PS****G" ref={this.inputRef}required/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" name="password" placeholder="password" required />
+                                <input type="password"id="isdd" name="password" placeholder="password" required />
                             </div>
                         </div>
 
                     </div>
                     <div className="footer2">
-                        <button type="button" className="btn" onClick={this.handlerclick} > <a href="" className="tt" target='_blank'>Login</a>
+                        <button type="button" className="btn"  id="btt" onClick={this.handlerclick} > Login
 
                         </button>
 
